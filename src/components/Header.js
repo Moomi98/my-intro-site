b3078cd86281e95b3f5084e99a4ffae89ffe9035
Moomi98 <kymkjh2002@gmail.com>
2022-09-17 20:40:23 +0900

    init commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100%;
+  height: 70px;
+  background-color: black;
+`;
+
+const Header = () => {
+  return <Container />;
+};
+
+export default Header;
