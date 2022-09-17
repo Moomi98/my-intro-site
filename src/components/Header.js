82b8df0b4fa53044b36c2712ede2a413813113df
Moomi98 <kymkjh2002@gmail.com>
2022-09-17 23:12:47 +0900

    header layout 적용

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,56 @@ const Container = styled.div`
   width: 100%;
   height: 70px;
   background-color: black;
+  position: fixed;
+`;
+
+const HeaderLayout = styled.div`
+  width: 75%;
+  height: 100%;
+  display: flex;
+  margin: 0 auto;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const MenuLayout = styled.li`
+  height: 100%;
+  display: flex;
+  gap: 15px;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+const Menu = styled.ul`
+  font-size: 18px;
+  font-weight: 600;
+  color: white;
+`;
+
+const Logo = styled.p`
+  font-size: 24px;
+  font-weight: bold;
+  color: white;
+  letter-spacing: 1.9px;
+  text-align: center;
 `;
 
 const Header = () => {
-  return <Container />;
+  return (
+    <Container>
+      <HeaderLayout>
+        <Logo>김정현</Logo>
+        <MenuLayout>
+          <Menu>Home</Menu>
+          <Menu>About</Menu>
+          <Menu>Skills</Menu>
+          <Menu>Resume</Menu>
+          <Menu>Projects</Menu>
+          <Menu>Contact</Menu>
+        </MenuLayout>
+      </HeaderLayout>
+    </Container>
+  );
 };
 
 export default Header;
